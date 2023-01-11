4481ca8ac5ef2b934b788a62d4c05cbe57158e9e
mrlsongzz <31463833+mrlsongzz@users.noreply.github.com>
2023-01-11 17:03:59 +0800

    Update sentence.js

--- sentence.js
+++ sentence.js
@@ -1,6 +1,6 @@
 $(function(){
     addLayout();
+    let addLayout = function () {
+        $("body").append("<div>测试内容</div>")
+    }
 })
-let addLayout = function () {
-    $("body").append("<div>测试内容</div>")
-}
