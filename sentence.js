54614cc28a0433bbd563ee50c07b2c881aa4d147
mrlsongzz <31463833+mrlsongzz@users.noreply.github.com>
2023-01-11 18:11:39 +0800

    Update sentence.js

--- sentence.js
+++ sentence.js
@@ -2,7 +2,7 @@ let addLayout = function () {
     let contianer = "<div class='contianer'>${content}</div>";
     let sentenceDiv = "<p class='sentence'><span class='sentence-content'>三十年众生牛马，六十年诸佛龙象</span></p>";
     contianer = contianer.replace("${content}",sentenceDiv);
-    $("#blog_head_bar").append(contianer);
+    $("#blog_head_bar").append("<div>11111111111</div>");
 }
 $(function(){
     addLayout();
