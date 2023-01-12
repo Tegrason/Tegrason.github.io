639ac398f531a58d209b6f82e4a76d50be8fe445
mrlsongzz <31463833+mrlsongzz@users.noreply.github.com>
2023-01-12 08:21:55 +0800

    Update sentence.js

--- sentence.js
+++ sentence.js
@@ -3,7 +3,6 @@ let addLayout = function () {
     let sentenceDiv = "<p class='sentence'><span class='sentence-content'>三十年众生牛马，六十年诸佛龙象</span></p>";
     contianer = contianer.replace("${content}",sentenceDiv);
     $("#keyword_searcher").append(contianer);
-    $("")
 }
 $(function(){
     addLayout();
