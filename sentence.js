374cc5a6fd55abdd07935ccc39ddec24942607f2
mrlsongzz <31463833+mrlsongzz@users.noreply.github.com>
2023-01-12 08:31:56 +0800

    Update sentence.js

--- sentence.js
+++ sentence.js
@@ -1,8 +1,18 @@
 let addLayout = function () {
-    let contianer = "<div class='contianer'>${content}</div>";
+
+    //添加诗句容器载体
+    let container = "<div class='container'>${content}</div>";
     let sentenceDiv = "<p class='sentence'><span class='sentence-content'>三十年众生牛马，六十年诸佛龙象</span></p>";
-    contianer = contianer.replace("${content}",sentenceDiv);
-    $("#blog_panel").append(contianer);
+    container = container.replace("${content}",sentenceDiv);
+    $("#blog_panel").append(container);
+
+    //计算容器位置
+    let targetContainer = $("#blog_panel");
+    let selfContainer = $(".container");
+    let y = $(targetContainer).height();
+    let x = $(targetContainer).width();
+    x = (x - selfContainer) / 2;
+    $(selfContainer).left = x;
 }
 $(function(){
     addLayout();
